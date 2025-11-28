85ea04d9c89babe8bd2a8a08bd32d2bd9ca8ae4c
mageshyt <mageshytdev@gmail.com>
2025-11-28 22:50:50 +0530

    Add QueryProvider component with React Query integration and update dependencies

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import QueryProvider from "@/components/providers/query-provider";
 import { SessionProvider } from "next-auth/react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -7,19 +8,21 @@ import "react-toastify/dist/ReactToastify.css";
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
-      {children}
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="dark"
-      />
+      <QueryProvider>
+        {children}
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+        />
+      </QueryProvider>
     </SessionProvider>
   );
 }
