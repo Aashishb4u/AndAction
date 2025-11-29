1b652bb6351c3b60befcdc9fa02ab16c2f3c20cb
mageshyt <mageshytdev@gmail.com>
2025-11-29 17:49:43 +0530

    Add VSCode settings for Prisma and update next.config.ts to include Instagram CDN patterns

--- next.config.ts
+++ next.config.ts
@@ -27,6 +27,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "*.cdninstagram.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
