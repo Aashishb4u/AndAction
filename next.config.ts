c77861f6aa32a7efff5b99fe1da6e86731557ddc
Manikandan0237 <sranjithkumar30052004@gmail.com>
2026-03-20 21:39:02 +0530

    Added configuration for media

--- next.config.ts
+++ next.config.ts
@@ -39,6 +39,12 @@ const nextConfig: NextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "media.andaction.in",
+        port: "",
+        pathname: "/uploads/**",
+      },
     ],
   },
 };
