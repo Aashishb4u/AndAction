dc5e62b28ac188c31a6abb20014dea3493dddd1a
mageshyt <mageshytdev@gmail.com>
2026-04-06 19:45:39 +0530

    fix(seed): update seed script to approve all videos

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -23,6 +23,19 @@ async function main() {
   console.log(
     `Deleted ${result.count} videos with youtubeVideoId containing "insta"`,
   );
+
+  // make all teh video to isApproved = true
+  const approvedResult = await prisma.video.updateMany({
+    where: {
+      isApproved: false,
+    },
+    data: {
+      isApproved: true,
+    },
+  });
+  console.log(
+    `Updated ${approvedResult.count} videos to be approved (isApproved = true)`,
+  );
 }
 
 main()
